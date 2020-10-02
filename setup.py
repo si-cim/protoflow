39a823602721fedcdfd77b006038ae121beb408f
Jensun Ravichandran <jjensun@gmail.com>
2020-10-02 19:00:20 +0200

    Update required tensorflow version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(name='protoflow',
       download_url=DOWNLOAD_URL,
       license='MIT',
       install_requires=[
-          'tensorflow>=2.2.0',
+          'tensorflow>=2.3.1',
           'numpy>=1.9.1',
           'sklearn',
       ],
