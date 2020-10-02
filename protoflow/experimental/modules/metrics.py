b9358a08169e19e079f42cb37ed4b64b880c60b0
Jensun Ravichandran <jjensun@gmail.com>
2020-10-02 17:41:13 +0200

    Remove experimental

--- protoflow/experimental/modules/metrics.py
+++
@@ -1,1 +0,0 @@
-"""ProtoFlow experimental metrics."""
--- protoflow/experimental/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .utils import basic_sanity_check, get_2d_lvqmln, get_basic_glvq
-
-__all__ = [
-    'basic_sanity_check', 'get_2d_lvqmln', 'get_basic_glvq',
-]
