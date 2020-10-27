98600d52f67ca43ed15407cc4e90333604346218
Jensun Ravichandran <jjensun@gmail.com>
2020-10-27 10:32:27 +0100

    Add normalization constraints for omega in GMLVQ

--- protoflow/modules/__init__.py
+++ protoflow/modules/__init__.py
@@ -1,15 +1,8 @@
-from .constraints import OmegaNormalization
-from .initializers import Eye
 from .losses import GLVQLoss
 from .metrics import accuracy_score, wtac_accuracy
-from .regularizers import LogDeterminant, OmegaRegularizer
 
 __all__ = [
-    'Eye',
-    'GLVQLoss',
-    'LogDeterminant',
-    'OmegaRegularizer',
-    'OmegaNormalization',
-    'accuracy_score',
-    'wtac_accuracy',
+    "GLVQLoss",
+    "accuracy_score",
+    "wtac_accuracy",
 ]
